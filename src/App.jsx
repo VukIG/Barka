00c8ec9b4baa6148c12cc7554ce6d42a86e39246
VukIG <vuk.ignjatovic05@gmail.com>
2026-07-29 10:15:12 +0200

    Creation of the initial react application using Vite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='flex bold m-10 text-green-600 font-bold text-4xl justify-center align-middle h-full'>SIVKA</div>
+  )
+}
+
+export default App
